8e16524e7662d17f893fe3defcc1831b8acd7fc2
Abof <dariusz.olszowy@gmail.com>
2017-01-14 17:00:24 +0100

    Make use of automatic way to create application.xml. Change packages for readability.

--- src/main/java/pl/abof/test_template/arquillian/wf/basic/EchoService.java
+++ src/main/java/pl/abof/test_template/arquillian/wf/basic/EchoService.java
@@ -1,13 +1,25 @@
-package com.github.abof.test.template.wildfly82_arquillian_basic;
+package pl.abof.test_template.arquillian.wf.basic;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Stream.generate;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Simple stateless service/bean for basic Arquillian test. Wanna see it in action -
+ * check {@link EchoServiceTest}
+ * 
+ * @author Abof
+ *
+ */
+@Stateless
+@LocalBean
 public class EchoService {
-	
+
 	private static final int NO_OF_ECHOES = 3;
 	private static final String RESONANCE_SOUND = "...";
-	
+
 	public String response(String sound) {
 		return generate(() -> sound.concat(RESONANCE_SOUND)).limit(NO_OF_ECHOES).collect(joining());
 	}
--- src/test/java/pl/abof/test_template/arquillian/wf/basic/EchoServiceTest.java
+++ src/test/java/pl/abof/test_template/arquillian/wf/basic/EchoServiceTest.java
@@ -1,37 +1,60 @@
-package com.github.abof.test.template.wildfly82_arquillian_basic;
+package pl.abof.test_template.arquillian.wf.basic;
 
 import static org.junit.Assert.*;
 
-import javax.inject.Inject;
+import java.io.IOException;
+
+import javax.ejb.EJB;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import pl.abof.test_template.arquillian.util.ear.AutoApplicationDescriptionBuilder;
+import pl.abof.test_template.arquillian.wf.basic.EchoService;
+
+
 @RunWith(Arquillian.class)
 public class EchoServiceTest {
 
-	@Inject
+	@EJB
 	private EchoService cut; // ClassUnderTests
-	
+
 	@Deployment
-	public static JavaArchive createDeplayment() {
-		return ShrinkWrap.create(JavaArchive.class, "testApp.jar")
-			.addClass(EchoService.class)
-			.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	public static Archive<?> createDeployment() throws IOException {
+		return getEar();
 	}
-	
+
 	@Test
 	public void testResponse() {
 		String testSound = "Hello";
 		String expectedResponseSound = "Hello...Hello...Hello...";
 		String actualResponse = cut.response(testSound);
-		
+
 		assertEquals(expectedResponseSound, actualResponse);
+	}
+
+	public static EnterpriseArchive getEar() throws IOException {
+		JavaArchive testEjbJar = ShrinkWrap
+				.create(JavaArchive.class, "test-ejb.jar")
+				.addClass(EchoService.class)
+				.addClass(EchoServiceTest.class);
+		
+		// EAR + HANDLER
+		AutoApplicationDescriptionBuilder descBuildHandler;
+		EnterpriseArchive ear = ShrinkWrap
+				.create(EnterpriseArchive.class)
+				.addHandlers(descBuildHandler = new AutoApplicationDescriptionBuilder())
+				.addAsModule(testEjbJar)
+				.setApplicationXML(descBuildHandler.getAppXmlAsset());	
+
+		return ear;
+
 	}
 
 }
