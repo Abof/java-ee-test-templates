fa12013f3a3333f9d67fb98c006e3fa5d618c98a
Abof <dariusz.olszowy@gmail.com>
2017-01-21 21:38:06 +0100

    Remove unused operations tracking feature ;)

--- src/main/java/pl/abof/test_template/arquillian/wf/cdi/Value.java
+++ src/main/java/pl/abof/test_template/arquillian/wf/cdi/Value.java
@@ -1,12 +1,8 @@
 package pl.abof.test_template.arquillian.wf.cdi;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Value {
 	
 	private int value = 0;
-	private List<String> processingHistory = new ArrayList<>();
 
 	public Value(int value) {
 		super();
@@ -21,11 +17,4 @@ public class Value {
 		this.value = value;
 	}
 
-	public List<String> getProcessingHistory() {
-		return processingHistory;
-	}
-
-	public void addProcessingHistoryEntry(String processingName) {
-		this.processingHistory.add(processingName);
-	}
 }
